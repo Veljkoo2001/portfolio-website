9cbf3c5d5f03bff344fa1e88c671f9efec2a00b5
Veljkoo2001 <veljkojakovljevic56@gmail.com>
2026-02-11 15:11:52 +0100

    Final local version for deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -137,17 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-import os
 
 if DEBUG:
     # Development: email samo ide u terminal
--- projects/managers.py
+++ projects/managers.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class ProjectManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().select_related('author').prefetch_related('tags')
+        return super().get_queryset()
